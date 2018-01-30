2c023f10bbcf8ab4a7ce37078d87bbe89e4d5c15
Elvisley Souza <elvisleysouza@MacBook-Pro-de-Elvisley.local>
2018-01-29 23:34:57 -0200

    Implementation repository

--- Marketplace.API.Domain/Domain/Marketplace.cs
+++ Marketplace.API.Domain/Domain/Marketplace.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Marketplace.API.Domain.Domain
 {
     public class Marketplace
     {
+        [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
 
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
+        [JsonProperty(PropertyName = "social_reason")]
         public string SocialReason { get; set; }
 
+        [JsonProperty(PropertyName = "address")]
         public string Address { get; set; }
 
+        [JsonProperty(PropertyName = "location_maps")]
         public string LocationMaps { get; set; }
 
+        [JsonProperty(PropertyName = "phone")]
         public int Phone { get; set; }
 
+        [JsonProperty(PropertyName = "photo")]
         public string Photo { get; set; }
 
     }
